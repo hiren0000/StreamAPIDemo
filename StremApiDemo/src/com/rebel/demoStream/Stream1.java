9e81f25a8c6f284955598ca81f1eff0101df667c
Hiren <rebeldevmurari@gmail.com>
2022-12-14 17:23:05 +0000

    added Stream API and used it to reduce the code

--- StremApiDemo/src/com/rebel/demoStream/Stream1.java
+++ StremApiDemo/src/com/rebel/demoStream/Stream1.java
@@ -2,7 +2,11 @@ package com.rebel.demoStream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Stream1 {
 
@@ -23,6 +27,39 @@ public class Stream1 {
 		
 		List<Integer> list3 = Arrays.asList(1,25,11,25,14,20,22,24,25);
 		
+		
+		//filtering without using Stream api
+		
+		//taking new list for even numbers
+		
+		List<Integer> Even = new ArrayList();
+		
+		
+		for(Integer i : list1)
+		{
+			if(i%2==0)
+			{
+				Even.add(i);
+			}
+		}
+		
+		System.out.println("Showing even numbers after filtering "+Even);
+		
+		
+		//Lets try to use it StreamAPI and filter function
+		
+		Stream<Integer> stream =list1.stream();
+		Collection<Integer> newList =stream.filter(i -> i%2==0).collect(Collectors.toList());
+		System.out.println(newList);
+		
+		/* we can still perform operation in one line
+		we did method chaining here so we can make our code more shorter
+		so in a line we can do filter and also gets the new list 
+		this is helpful for reducing the length of the code 
+		*/
+		
+		Collection<Integer> list=list1.stream().
+				       filter(i -> i % 2 == 0).collect(Collectors.toList());
 	}
 
 }
